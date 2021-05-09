9d2dff78e5410593eda326fa908b8a55c2bf1b08
thebootsie123 <bootsie227@gmail.com>
2021-05-09 12:43:42 -0400

    Made scrollbar blend in more when fullscreen

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,25 @@
     width: 5px;
     background: transparent;
   }
+
   ::-webkit-scrollbar-track {
     -webkit-border-radius: 10px;
     border-radius: 10px;
   }
+
   ::-webkit-scrollbar-thumb {
     background-color: #cccacc;
     -webkit-border-radius: 10px;
     border-radius: 10px;
+  }
+
+  @media all and (display-mode: fullscreen) {
+    ::-webkit-scrollbar {
+      background: black;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: #232323;
+    }
   }
 </style>
