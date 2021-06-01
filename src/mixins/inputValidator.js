fb02f7e846a74ffac5087232902f6e1950b40225
bootsie123 <bootsie227@gmail.com>
2021-06-01 07:50:31 -0400

    Fixed email validation for emails with more than one TLD

--- src/mixins/inputValidator.js
+++ src/mixins/inputValidator.js
@@ -9,12 +9,9 @@ export default {
         const emailName = emailParts[0];
         const emailDomain = emailParts[1].split(".");
 
-        if (
-          emailName.length <= 0 ||
-          emailDomain.length !== 2 ||
-          emailDomain[0].length <= 0 ||
-          emailDomain[1].length <= 0
-        ) {
+        if (emailDomain.includes("")) return false;
+
+        if (emailName.length < 1 || emailDomain.length < 2) {
           return false;
         }
       }
